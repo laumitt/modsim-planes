e2099420f5ed578be0e66f3a8a860b115eb29243
Laurel Mitton <lmitton@olin.edu>
2019-09-24 10:51:14 -0400

    end of day 1, pseudocode beginning

--- planes.py
+++ planes.py
@@ -1,1 +1,12 @@
-print('hello world')
+class Airport(self, code, size, destinations):
+    self.code = code
+
+class Plane(self, number, breakage, loc, dest, eta):
+    self.number = number
+
+def check_terminal():
+    if terminal_free == True:
+        plane_terminal = next_terminal
+    else:
+        plane_terminal = terminal_0
+    
